74b95303f8c47f03497b75e478b4422ac45cf3c8
Kenya <kenya55@msn.com>
2019-08-25 16:13:48 -0400

    added submit button to index file, added firebase config and intialized

---
+++ javascript/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCZMUBxeTw2GLOmVO1dbsX8DP12OxKH-xY",
+  authDomain: "train-scheduler-9280e.firebaseapp.com",
+  databaseURL: "https://train-scheduler-9280e.firebaseio.com",
+  projectId: "train-scheduler-9280e",
+  storageBucket: "",
+  messagingSenderId: "1038050892736",
+  appId: "1:1038050892736:web:e522553a201e500d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
